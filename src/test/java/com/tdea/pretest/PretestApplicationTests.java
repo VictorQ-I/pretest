d6d166fa1acbdd7e8f2b3a07f47d68980cffc7e6
Morales1429 <Nimatis@hotmail.com>
2022-03-19 11:44:21 -0500

    Update PretestApplicationTests.java

--- src/test/java/com/tdea/pretest/PretestApplicationTests.java
+++ src/test/java/com/tdea/pretest/PretestApplicationTests.java
@@ -2,10 +2,19 @@ package com.tdea.pretest;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 class PretestApplicationTests {
 
     @Test
     void contextLoads() {
+        String contra="123456";
+        String password="123456";
+
+        boolean valor = UserUtils.isPasswordEqual(contra, password); //con este metodo importamos el UserUtils para comparar las dos contraseñas y asignamos un nombre al metodo
+
+        assertTrue(valor); //de esta forma ejecutamos el true para comprobar si es verdadero o falso el metodo anterior
     }
+
 
 }
