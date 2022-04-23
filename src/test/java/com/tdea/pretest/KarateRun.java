320a172e5c06f5b0f2f64a0b8502be98dbde99d0
VictorQ-I <68793547+VictorQ-I@users.noreply.github.com>
2022-04-23 16:43:44 -0500

    ok
    ok

--- src/test/java/com/tdea/pretest/KarateRun.java
+++ src/test/java/com/tdea/pretest/KarateRun.java
@@ -3,7 +3,7 @@ import com.intuit.karate.junit5.Karate;
 
 public class KarateRun {
         @Karate.Test
-        Karate testAll() {
+        Karate testKarate() {
             return Karate.run("classpath:com.tdea.pretest/karate/");
         }
 
